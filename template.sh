f1562f988b88bd9e9f5144e3dd398e4cc9d85597
Toon Verbeek <toon.verbeek1@gmail.com>
2016-04-25 16:00:54 +0200

    cp instead of mv

--- template.sh
+++ template.sh
@@ -13,25 +13,25 @@ cat > $APP_NAME.json <<EOF
                 {
                     "containerPort": $CONTAINER_PORT,
                     "hostPort": 0,
-										"servicePort": 10000,
+                    "servicePort": 10000,
                     "protocol": "tcp"
                 }
             ]
         }
     },
     "healthChecks": [{
-			"protocol": "HTTP",
-			"portIndex": 0
+          "protocol": "HTTP",
+          "portIndex": 0
     }],
-		"labels":{
-				"HAPROXY_GROUP":"external",
-				"HAPROXY_0_VHOST":"$MESOS_DNS_HOST",
-				"HAPROXY_0_PORT": "80"
-		},
-		"cmd": "until rake cassandra:setup; do sleep 5; done && rails server",
+     "labels":{
+          "HAPROXY_GROUP":"external",
+          "HAPROXY_0_VHOST":"$MESOS_DNS_HOST",
+          "HAPROXY_0_PORT": "80"
+    },
+    "cmd": "until rake cassandra:setup; do sleep 5; done && rails server",
     "ports": [0],
     "cpus": 0.25,
     "mem": 256.0,
-		"working-dir": "/rails"
+    "working-dir": "/rails"
 }
 EOF
