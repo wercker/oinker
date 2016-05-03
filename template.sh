579590e1b7b2b258d8683ada6e1901e9fcf37a17
Toon Verbeek <toon.verbeek1@gmail.com>
2016-05-03 14:09:27 +0200

    update template

--- template.sh
+++ template.sh
@@ -28,6 +28,10 @@ cat > $APP_NAME.json <<EOF
           "HAPROXY_0_VHOST":"$MESOS_DNS_HOST",
           "HAPROXY_0_PORT": "80"
     },
+    "env":{
+			"SECRET_KEY_BASE": "be6ea21bd0e8ddad06accbdfbfcbc6f120815744a8177fb1196442c1670401c86a1d020f1fb62f9b7d6bacc8cf818de277d23d3f3e7dcf704ca88965e5b9ed86",
+			"CASSANDRA_HOSTS": "node-0.cassandra.mesos"
+    },
     "cmd": "until bundle exec rake cassandra:setup; do sleep 5; done && rails server",
     "ports": [0],
     "cpus": 0.25,
