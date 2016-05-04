3376f3872da7a79c9722fddb3635ff10802a2867
Toon Verbeek <toon.verbeek1@gmail.com>
2016-05-04 09:45:54 +0200

    update template

--- template.sh
+++ template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat > $APP_NAME.json <<EOF
-{
+[{
     "id": "/$APP_NAME",
     "instances": $INSTANCES,
     "container": {
@@ -29,13 +29,13 @@ cat > $APP_NAME.json <<EOF
           "HAPROXY_0_PORT": "80"
     },
     "env":{
-			"SECRET_KEY_BASE": "be6ea21bd0e8ddad06accbdfbfcbc6f120815744a8177fb1196442c1670401c86a1d020f1fb62f9b7d6bacc8cf818de277d23d3f3e7dcf704ca88965e5b9ed86",
-			"CASSANDRA_HOSTS": "node-0.cassandra.mesos"
+          "SECRET_KEY_BASE": "be6ea21bd0e8ddad06accbdfbfcbc6f120815744a8177fb1196442c1670401c86a1d020f1fb62f9b7d6bacc8cf818de277d23d3f3e7dcf704ca88965e5b9ed86",
+          "CASSANDRA_HOSTS": "node-0.cassandra.mesos"
     },
     "cmd": "until bundle exec rake cassandra:setup; do sleep 5; done && rails server",
     "ports": [0],
     "cpus": 0.25,
     "mem": 256.0,
     "working-dir": "/rails"
-}
+}]
 EOF
